dd18848984e1b35e484ce350a63d2c2f78fabe22
tei-k <tei@leosys.space>
2020-12-22 11:53:27 +0900

    Add examples

---
+++ aws/monitor/main.tf
@@ -0,0 +1,17 @@
+module "monitor" {
+  source = "git@github.com:scalar-labs/scalar-terraform.git//modules/aws/monitor?ref=master"
+
+  # Required Variables (Use remote state)
+  network   = local.network
+  cassandra = local.cassandra
+  scalardl  = local.scalardl
+
+  # Optional Variables
+  base    = var.base
+  monitor = var.monitor
+  targets = var.targets
+
+  slack_webhook_url = var.slack_webhook_url
+
+  custom_tags = local.custom_tags
+}
---
+++ aws/scalardl/main.tf
@@ -0,0 +1,14 @@
+module "scalardl" {
+  source = "git@github.com:scalar-labs/scalar-terraform.git//modules/aws/scalardl?ref=master"
+
+  # Required Variables (Use remote state)
+  network   = local.network
+  cassandra = local.cassandra
+
+  # Optional Variables
+  base     = var.base
+  scalardl = var.scalardl
+  envoy    = var.envoy
+
+  custom_tags = local.custom_tags
+}
