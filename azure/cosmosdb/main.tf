ab7c1269c32c5e1da2fc4b04e1b170a5a882ecaf
Yusuke Morimoto <yusuke.morimoto@gmail.com>
2021-01-07 10:43:21 +0900

    Use the remote cosmosdb module source instead of a relative path

--- azure/cosmosdb/main.tf
+++ azure/cosmosdb/main.tf
@@ -1,6 +1,5 @@
 module "cosmosdb" {
-  # source = "git::https://github.com/scalar-labs/scalar-terraform.git//modules/azure/cosmosdb?ref=master"
-  source = "../../../modules/azure/cosmosdb"
+  source = "git::https://github.com/scalar-labs/scalar-terraform.git//modules/azure/cosmosdb?ref=master"
 
   # Required Variables
   network            = local.network
