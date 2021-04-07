6b27f70d4f1fb6ba479ba5f5707692717f01dedf
Yusuke Morimoto <yusuke.morimoto@gmail.com>
2021-04-07 16:48:33 +0900

    terraform fmt

--- aws/scalardl/main.tf
+++ aws/scalardl/main.tf
@@ -2,7 +2,7 @@ module "scalardl" {
   source = "git::https://github.com/scalar-labs/scalar-terraform.git//modules/aws/scalardl?ref=master"
 
   # Required Variables (Use remote state)
-  network   = local.network
+  network = local.network
 
   # Optional Variables
   base     = var.base
--- azure/scalardl/main.tf
+++ azure/scalardl/main.tf
@@ -2,7 +2,7 @@ module "scalardl" {
   source = "git::https://github.com/scalar-labs/scalar-terraform.git//modules/azure/scalardl?ref=master"
 
   # Required Variables (Use remote state)
-  network   = local.network
+  network = local.network
 
   # Optional Variables
   base     = var.base
